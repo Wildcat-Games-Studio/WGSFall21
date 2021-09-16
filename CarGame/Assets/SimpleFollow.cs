a505fd81c6db7a5f5715dfd435674effec963b7a
Ben Biles <bbiles@stny.rr.com>
2021-09-16 16:04:21 -0400

    Initlial Commit
    Unity project created along with basic car movement

---
+++ CarGame/Assets/SimpleFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
