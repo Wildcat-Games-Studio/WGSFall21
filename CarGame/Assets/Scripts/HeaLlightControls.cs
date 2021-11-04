ae57266db86f8e4c9d5ee226513bf405d753734b
Ethan Lehutsky <etl4@pct.edu>
2021-11-04 15:41:05 -0400

    Car
    Added the car for our racing game

---
+++ CarGame/Assets/Scripts/HeaLlightControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeaLlightControls : MonoBehaviour
+{
+
+    private Light headLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        headLight = gameObject.GetComponent<Light>();
+        print(headLight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            if (headLight.enabled)
+            {
+                headLight.enabled = false;
+            }
+            else
+            {
+                headLight.enabled = true;
+            }
+            
+        }
+    }
+}
