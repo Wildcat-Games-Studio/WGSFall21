8ebc647d45195dba519273d85fa67cf901562fcb
Ben Biles <benjaminbiles01@gmail.com>
2021-11-18 16:10:51 -0500

    Racetrack one is a complete level
    Racetrack one has the checkpoint system set-up along with the timer and a new count down to start.

---
+++ CarGame/Assets/Scripts/RaceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RaceManager : MonoBehaviour
+{
+    public TimerManager timerManager;
+    public CarController carController;
+
+    public GameObject readyText;
+    public GameObject goText;
+
+    private float m_countdown = 0;
+
+    private void Update()
+    {
+
+        if (m_countdown >= 3.0f)
+        {
+            timerManager.SetCanTick(true);
+            carController.SetCanMove(true);
+            readyText.SetActive(false);
+            goText.SetActive(true);
+
+            Destroy(goText, 1.0f);
+            // Don't care :'(
+            Destroy(this);
+        }
+        else
+        {
+            m_countdown += Time.deltaTime;
+        }
+    }
+}
