91c1099fecf466575518eec83a4be3b88b9cac86
Troy Records Jr <giants2110@gmail.com>
2021-12-02 16:16:49 -0500

    Main Menu and Second track added.
    Main Menu scene and corresponding UI has been added. The Options menu has been converted to a prefab for reusability. Second scene track was added into the "Scenes/TrackTwo". Track was dragged in as FBX.

---
+++ CarGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Canvas MainMenuCanvas;
+    public Canvas OptionsMenuCanvas;
+
+    public void BeginBtnClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToggleOptionsMenu()
+    {
+        if (MainMenuCanvas == null)
+        {
+            Debug.Log("MainMenuCanvas is NULL. Cannot disable.");
+            return;
+        }
+        if (OptionsMenuCanvas == null)
+        {
+            Debug.Log("OptionsMenuCanvas is NULL. Cannot display.");
+            return;
+        }
+
+        MainMenuCanvas.enabled = !MainMenuCanvas.enabled;
+        OptionsMenuCanvas.enabled = !OptionsMenuCanvas.enabled;
+    }
+
+    public void QuitBtnClicked()
+    {
+        Application.Quit();
+    }
+}
